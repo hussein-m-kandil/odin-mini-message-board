92a56e3e9bf921c2b6fd5267da4f098b7c2c532c
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-09-12 15:51:00 +0300

    Move the messages to 'model' directory

---
+++ model/messages.js
@@ -0,0 +1,14 @@
+const messages = [
+  {
+    text: "Hi there!",
+    userName: "Superman",
+    date: new Date(),
+  },
+  {
+    text: "Hello World!",
+    userName: "Batman",
+    date: new Date(),
+  },
+];
+
+export default messages;
