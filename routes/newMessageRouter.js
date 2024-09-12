23deae845a50ca7d93fdcce4bde6eeca51c937cc
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-09-12 17:19:20 +0300

    Use controllers to add new message on '/new' route

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,9 +1,21 @@
 import express from "express";
+import messageController from "../controllers/messageController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("<h1>Here you can add new message</h1>");
+router.use("/", (req, res, next) => {
+  if (req.query.err) {
+    if (!Array.isArray(req.query.err)) {
+      res.locals.errorMessage = req.query.err;
+    }
+  }
+  next();
 });
+
+router.get("/", messageController.getNewMessageFrom);
+
+router.use(express.urlencoded({ extended: true }));
+
+router.post("/", messageController.createNewMessage);
 
 export default router;
