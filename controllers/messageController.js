80d1be59e63905a2938a483d3b112b56917e1ff4
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-09-12 17:18:51 +0300

    Add view & controllers for adding new message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -9,4 +9,30 @@ export default {
       messages,
     });
   },
+
+  getNewMessageFrom(req, res) {
+    res.render("new-message", { title: "Add New Message" });
+  },
+
+  createNewMessage(req, res) {
+    const newMessage = {
+      userName: req.body["user-name"],
+      text: req.body.message,
+      date: new Date(),
+    };
+    let errorMessage;
+    if (newMessage.userName && newMessage.text) {
+      if (newMessage.userName.length > 28) {
+        errorMessage = encodeURIComponent("Your name is too long!");
+      }
+    } else {
+      errorMessage = encodeURIComponent("All fields are required!");
+    }
+    if (errorMessage) {
+      res.redirect(`${req.originalUrl}?err=${errorMessage}`);
+    } else {
+      messages.push(newMessage);
+      res.redirect("/");
+    }
+  },
 };
