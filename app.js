2d6b330481cb6452bca7d4463973c4ca02b0cd4e
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-09-12 14:30:08 +0300

    Add the main page view and render it on GET '/'

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+import path from "path";
 import process from "process";
 import express from "express";
 import messageBoardRouter from "./routes/messageBoardRouter.js";
 import newMessageRouter from "./routes/newMessageRouter.js";
 
 const PORT = process.env.PORT || 3000;
+const PUBLIC_DIR = path.join(process.cwd(), "public");
 
 const app = express();
+
+app.set("views", path.join(process.cwd(), "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(PUBLIC_DIR));
 
 app.use((req, res, next) => {
   console.log(`${req.method}: ${req.url}`);
--- routes/messageBoardRouter.js
+++ routes/messageBoardRouter.js
@@ -1,9 +1,27 @@
 import express from "express";
+import { formatDistanceToNow } from "date-fns";
+
+const messages = [
+  {
+    text: "Hi there!",
+    userName: "Superman",
+    date: new Date(),
+  },
+  {
+    text: "Hello World!",
+    userName: "Batman",
+    date: new Date(),
+  },
+];
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("<h1>Welcome to Odin Mini Message Board</h1>");
+  res.render("index", {
+    title: "Odin Mini Message Board",
+    formatDate: formatDistanceToNow,
+    messages,
+  });
 });
 
 export default router;
