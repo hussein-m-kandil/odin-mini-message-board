980b689714d0644efef41113f1f2a912b5b7fced
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-10-18 18:45:44 +0300

    Use PostgreSQL instead of MongoDB & Edit UI style

--- routes/messageBoardRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import messageController from "../controllers/messageController.js";
-
-const router = express.Router();
-
-router.get("/", messageController.getAllMessages);
-
-export default router;
